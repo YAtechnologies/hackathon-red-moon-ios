e527eaa4db865a1f442526b74dc254900e9f4957
ThabetRebhi <thabet.rebhi@gmail.com>
2022-11-10 17:09:39 +0100

    Initial Commit

---
+++ hackathon-red-moon-iosUITests/hackathon_red_moon_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  hackathon_red_moon_iosUITestsLaunchTests.swift
+//  hackathon-red-moon-iosUITests
+//
+//  Created by S3W on 10/11/2022.
+//
+
+import XCTest
+
+class hackathon_red_moon_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

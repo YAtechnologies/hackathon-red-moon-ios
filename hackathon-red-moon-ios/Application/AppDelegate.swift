90b14e6dcb9e06f7172461ccc5f2c70e7336ed37
ThabetRebhi <thabet.rebhi@gmail.com>
2022-11-10 21:52:18 +0100

    add login and logout to app delegate

--- hackathon-red-moon-ios/Application/AppDelegate.swift
+++ hackathon-red-moon-ios/Application/AppDelegate.swift
@@ -8,18 +8,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        do {
+            try AuthHelper.initialize()
+        } catch {
+            fatalError("Unable to initialize `AuthHelper`, error: \(error.localizedDescription)")
+        }
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         let LaunchVC = LaunchVC()
         LaunchVC.delegate = self
         self.window?.rootViewController = LaunchVC
         window?.makeKeyAndVisible()
+        
         return true
+    }
+    
+    func logout() {
+        do{
+            try AuthHelper.shared.deletePersistedAccount()
+        }catch {
+            fatalError(error.localizedDescription)
+        }
     }
     
     func navigateToNewRoot(isLoggedIn: Bool) {
         let SignVC = SignVC()
         SignVC.delegate = self
+        
         let vc = isLoggedIn ? TabBarController() : SignVC
+        if !isLoggedIn {logout()}
+        
         let navigationController = UINavigationController(rootViewController: vc)
         navigationController.navigationBar.isHidden = true
         
